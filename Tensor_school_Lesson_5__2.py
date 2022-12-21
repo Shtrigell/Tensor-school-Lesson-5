dd44c63fb899ca9fefa58552426a853d1d081759
Shtrigell <119353277+Shtrigell@users.noreply.github.com>
2022-12-21 19:47:19 +0300

    Update Tensor_school_Lesson_5__2.py

--- Tensor_school_Lesson_5__2.py
+++ Tensor_school_Lesson_5__2.py
@@ -14,9 +14,9 @@ sum_list=[]
 sum_list = input("Введите числа, через пробел, которые хотите сложить. Для выхода нахмите Enter. ").split()
 for i in range (len(sum_list)):
     try:
-        sum_list[i]=int(sum_list[i])
+        sum_list[i] = int(sum_list[i])
     except ValueError:
         print("Неверный формат данных, данные будут удалены.")
-        sum_list[i]=0
+        sum_list[i] = 0
 a=summary(*sum_list)
 print("Ваша сумма: ", a)
